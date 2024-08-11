a6e078de868dafa9aa18dcb5151af311b58cbd76
yushibatan <y-shibata@tokyo-gas.co.jp>
2024-08-11 15:56:54 +0900

    課題１２をアップロード

---
+++ kadai_012/crud/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.urls import reverse
+
+class Detail(models.Model):
+     name = models.TextField(blank=True, null=True)
+ 
+     def __str__(self):
+         return self.name
+
+
+class Category(models.Model):
+     name = models.CharField(max_length=200)
+ 
+     def __str__(self):
+         return self.name
+
+class Product(models.Model):
+     name = models.CharField(max_length=200)
+     price = models.PositiveIntegerField()
+     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+     img = models.ImageField(blank=True, default='noImage.png')
+     detail = models.ForeignKey(Detail, on_delete=models.CASCADE)
+
+     def __str__(self):
+         return self.name
+
+    # 新規作成・編集完了時のリダイレクト先
+     def get_absolute_url(self):
+         return reverse('list')
